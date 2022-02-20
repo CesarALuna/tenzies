e665a87a25327c6f4854bc9a582a75c63aac543c
cesar <ceslun@gmail.com>
2022-02-19 18:56:47 -0800

    Init and style Dice component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import Dice from './components/Dice'
+
 export default function App() {
-  return <main></main>
+  return (
+    <main>
+      <div className="dice--container">
+        <Dice value="1" />
+        <Dice value="2" />
+        <Dice value="3" />
+        <Dice value="4" />
+        <Dice value="5" />
+        <Dice value="6" />
+        <Dice value="7" />
+        <Dice value="8" />
+        <Dice value="9" />
+        <Dice value="10" />
+      </div>
+    </main>
+  )
 }
